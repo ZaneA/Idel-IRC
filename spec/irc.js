e730605e4c1de67d8a3a45f9ab1db78c697b41fc
Zane Ashby <zane.a@demonastery.org>
2013-06-09 17:56:58 +1200

    Update names of tests to correspond to IRC replies.

--- spec/irc.js
+++ spec/irc.js
@@ -12,8 +12,8 @@ describe('IRC Connection', function () {
     };
   }));
   
-  describe('353 Names', function () {
-    it('should add correct nicks to channel list', function () {
+  describe('RFC1459::353::RPL_NAMREPLY', function () {
+    it('should add nicks to channel nicklist', function () {
       network.onMessage(':irc.demonastery.org 353 me @ #mock :me @you');
       expect(network.channels[0].nicks[0].name).toBe('me');
       expect(network.channels[0].nicks[0].mode).toBe('');
@@ -22,21 +22,21 @@ describe('IRC Connection', function () {
     });
   });
   
-  describe('332 Topic', function () {
+  describe('RFC1459::332::RPL_TOPIC', function () {
     it('should update the topic variable', function () {
       network.onMessage(':irc.demonastery.org 332 me #mock :new subject :D');
       expect(network.channels[0].topic).toBe('new subject :D');
     });
   });
 
-  describe('TOPIC', function () {
+  describe('RFC1459::TOPIC', function () {
     it('should update the topic variable', function () {
       network.onMessage(':you!user@host TOPIC #mock :new subject :D');
       expect(network.channels[0].topic).toBe('new subject :D');
     });
   });
   
-  describe('376|422 End of MOTD', function () {
+  describe('RFC1459::376::RPL_ENDOFMOTD|RFC1459::422::ERR_NOMOTD', function () {
     it('should join any joinChannels', function () {
       network.joinChannels = ['#one', '#two'];
       network.onMessage(':irc.demonastery.org 376 me :End of /MOTD command.');
@@ -44,15 +44,15 @@ describe('IRC Connection', function () {
     });
   });
   
-  describe('433 Nick in use', function () {
+  describe('RFC1459::433::ERR_NICKNAMEINUSE', function () {
     it('should modify nick, and re-nick', function () {
-      network.onMessage(':irc.demonastery.org 433 me :Nick in use');
+      network.onMessage(':irc.demonastery.org 433 me :Nickname is already in use');
       expect(network.nick.name).toBe('me_');
       expect(network.lineBuffer).toContain('NICK me_');
     });
   });
   
-  describe('JOIN', function () {
+  describe('RFC1459::JOIN', function () {
     it('should handle ourselves joining', function () {
       network.onMessage(':me!user@host JOIN :#Channel');
       expect(network.channels[1].name).toBe('#Channel');
@@ -64,7 +64,7 @@ describe('IRC Connection', function () {
     });
   });
   
-  describe('PART with extra colon', function () {
+  describe('RFC1459::PART with extra colon', function () {
     it('should handle ourselves leaving', function () {
       network.onMessage(':me!user@host PART :#mock');
       expect(network.channels[0]).toBeUndefined();
@@ -77,7 +77,7 @@ describe('IRC Connection', function () {
     });
   });
 
-  describe('PART without extra colon', function () {
+  describe('RFC1459::PART without extra colon', function () {
     it('should handle ourselves leaving', function () {
       network.onMessage(':me!user@host PART #mock');
       expect(network.channels[0]).toBeUndefined();
@@ -90,14 +90,14 @@ describe('IRC Connection', function () {
     });
   });
   
-  describe('PING', function () {
+  describe('RFC1459::PING', function () {
     it('should reply with PONG', function () {
       network.onMessage('PING :test');
       expect(network.lineBuffer).toContain('PONG :test');
     });
   });
   
-  describe('PRIVMSG', function () {
+  describe('RFC1459::PRIVMSG', function () {
     it('should add the message to a buffer', function () {
       network.channels[0].nicks.push(_Nick('you'));
       network.onMessage(':you!user@host PRIVMSG #mock :Here is my' + ' message :)');
@@ -106,12 +106,20 @@ describe('IRC Connection', function () {
     });
   });
 
-  describe('NOTICE', function () {
+  describe('RFC1459::NOTICE', function () {
     it('should add the message to a buffer', function () {
       network.channels[0].nicks.push(_Nick('you'));
       network.onMessage(':you!user@host NOTICE #mock :Here is my' + ' message :)');
       expect(network.channels[0].buffer[1].nick.name).toBe('you');
       expect(network.channels[0].buffer[1].message).toBe('Here is my message :)');
+    });
+  });
+  
+  describe('RFC1459::352::RPL_WHOREPLY', function () {
+    it('should update the nicklist of a channel', function () {
+      network.onMessage(':irc.demonastery.org 352 me #mock ~user host server you H :0 realname');
+      expect(network.channels[0].nicks[0].name).toBe('you');
+      expect(network.channels[0].nicks[0].mode).toBe('');
     });
   });
 });
