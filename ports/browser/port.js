c4a10b6684aac881a2bc0ac1fd7d094b3f8cc4c6
Zane Ashby <zane.a@demonastery.org>
2013-06-10 19:26:56 +1200

    Add browser port. Not really functional yet.

---
+++ ports/browser/port.js
@@ -0,0 +1,32 @@
+app.service('PortService', function () {
+  this.canMinimize = false;
+  this.minimizeWindow = function () {};
+  
+  this.canClose = true;
+  this.closeWindow = function () {
+    window.close();
+  };
+  
+  this.saveSettings = function (obj, callback) {
+    for (var k in obj) {
+      window.localStorage.setItem(k, JSON.stringify(obj[k]));
+      callback();
+    }
+  };
+  
+  // Broken
+  this.loadSettings = function (key, def, callback) {
+    var obj = JSON.parse(window.localStorage.getItem(key));
+    callback(_.assign(obj, def));
+  };
+  
+  this.clearSettings = function (key, callback) {
+    if (key == null) {
+      window.localStorage.clear();
+    } else {
+      window.localStorage.removeItem(key);
+    }
+    
+    callback();
+  };
+});
