ffb35e9204ec09ebf5e26da21dcedc5106558378
Zane Ashby <zane.a@demonastery.org>
2013-06-16 16:04:56 +1200

    Add window frame back in by default, looks much better.

--- ports/chrome/background.js
+++ ports/chrome/background.js
@@ -4,11 +4,10 @@ chrome.app.runtime.onLaunched.addListener(function () {
   chrome.app.window.create('main.html', {
     id: 'idel',
     bounds: {
-      width: 800,
-      height: 400
+      width: 840,
+      height: 380
     },
     minWidth: 500,
-    minHeight: 200,
-    frame: 'none'
+    minHeight: 200
   });
 });
