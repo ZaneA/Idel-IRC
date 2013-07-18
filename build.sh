8f1d2bf1d1f0c77ada7834cecbc002902dab5c71
Zane Ashby <zane.a@demonastery.org>
2013-07-18 21:19:17 +1200

    Use minified version of lodash, seems to fix problems with CSP.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -9 -xports/chrome/manifest.json -r idel.zip manifest.json LICENSE main.html themes ports/chrome components/{angular-unstable/angular.min.js,less.js/dist/less-1.4.0-beta.min.js,lodash/lodash.js,moment/min/moment.min.js,underscore.string/dist/underscore.string.min.js} layouts css js assets
+zip -9 -xports/chrome/manifest.json -r idel.zip manifest.json LICENSE main.html themes ports/chrome components/{angular-unstable/angular.min.js,less.js/dist/less-1.4.0-beta.min.js,lodash/dist/lodash.min.js,moment/min/moment.min.js,underscore.string/dist/underscore.string.min.js} layouts css js assets
